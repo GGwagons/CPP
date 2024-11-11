30e24db74f2251e2eb8f546a767ebcf619e57138
Miha Turk <miturk@c3r10p1.42vienna.com>
2024-11-11 16:37:54 +0100

    still working on it

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,13 +6,13 @@
 /*   By: miturk <miturk@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 11:18:34 by miturk            #+#    #+#             */
-/*   Updated: 2024/11/11 16:26:47 by miturk           ###   ########.fr       */
+/*   Updated: 2024/11/11 16:37:17 by miturk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter() : _input("nanf") {
+ScalarConverter::ScalarConverter() : _input("What") {
 	std::cout << "Default constructor called" << std::endl;
 }
 
@@ -36,23 +36,30 @@ ScalarConverter::~ScalarConverter() {
 	std::cout << "Destructor called" << std::endl;
 }
 
-// void ScalarConverter::toChar(std::string &input) {
-// 	std::cout << "char: ";
-// }
+void ScalarConverter::toChar(std::string &input) {
+	std::cout << "char: ";
+}
 
-// void ScalarConverter::toInt(std::string &input) {
-// 	std::cout << "int: ";
-// }
+void ScalarConverter::toInt(std::string &input) {
+	std::cout << "int: ";
+}
 
-// void ScalarConverter::toFloat(std::string &input) {
-// 	std::cout << "float: ";
-// }
+void ScalarConverter::toFloat(std::string &input) {
+	std::cout << "float: ";
+}
 
-// void ScalarConverter::toDouble(std::string &input) {
-// 	std::cout << "double: ";
-// }
+void ScalarConverter::toDouble(std::string &input) {
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == '.')
+			std::cout << "double: ";
+	}
+	std::cout << "double: ";
+}
 
-void ScalarConverter::convert() {
+void ScalarConverter::convert(std::string &input) {
 	std::cout << "convert called" << std::endl;
-	
+	ScalarConverter::toChar(input);
+	ScalarConverter::toInt(input);
+	ScalarConverter::toFloat(input);
+	ScalarConverter::toDouble(input);
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: miturk <miturk@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 11:18:36 by miturk            #+#    #+#             */
-/*   Updated: 2024/11/11 16:26:18 by miturk           ###   ########.fr       */
+/*   Updated: 2024/11/11 16:35:38 by miturk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@
 class ScalarConverter {
 	private:
 		std::string _input;
-		// static void toChar(std::string &input);
-		// static void toInt(std::string &input);
-		// static void toFloat(std::string &input);
-		// static void toDouble(std::string &input);
-		static void convert();
+		static void toChar(std::string &input);
+		static void toInt(std::string &input);
+		static void toFloat(std::string &input);
+		static void toDouble(std::string &input);
 	public:
 		ScalarConverter();
 		ScalarConverter(std::string &input);
 		~ScalarConverter();
 		ScalarConverter(const ScalarConverter &copy);
 		ScalarConverter &operator=(const ScalarConverter &copy);
+		static void convert(std::string &input);
 };
 #endif
